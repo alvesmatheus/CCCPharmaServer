fb93817d7a6da9326be644bae48b82901c39987b
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2018-11-25 17:33:23 -0300

    Fix spacing

--- cccpharma/src/main/java/br/edu/ufcg/cccpharma/user/Authority.java
+++ cccpharma/src/main/java/br/edu/ufcg/cccpharma/user/Authority.java
@@ -8,41 +8,40 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-
 @Entity
-@Table(name="tb_authority")
-public class Authority{
+@Table(name = "tb_authority")
+public class Authority {
 
-    /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
 	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 
-    @Enumerated(EnumType.STRING)
-    private UserRoleName name;
+	@Enumerated(EnumType.STRING)
+	private UserRoleName name;
 
-    public String getAuthority() {
-        return name.name();
-    }
+	public String getAuthority() {
+		return name.name();
+	}
 
-    public void setName(UserRoleName name) {
-        this.name = name;
-    }
+	public void setName(UserRoleName name) {
+		this.name = name;
+	}
 
-    public UserRoleName getName() {
-        return name;
-    }
+	public UserRoleName getName() {
+		return name;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 }

1d88378f62ab9b1b430b0dc6b01f27f76135c208
FannyVieira <fanny.vieira@ccc.ufcg.edu.br>
2018-11-28 07:53:27 -0300

    Fix authentication error

--- cccpharma/src/main/java/br/edu/ufcg/cccpharma/security/WebSecurityConfig.java
+++ cccpharma/src/main/java/br/edu/ufcg/cccpharma/security/WebSecurityConfig.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -36,4 +38,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         	.apply(new JwtTokenFilterConfigurer(jwtTokenProvider));
 	}
 	
+	
+	@Override
+    public void configure(WebSecurity web) throws Exception {
+        // AuthenticationTokenFilter will ignore the below paths
+        web.ignoring().antMatchers(HttpMethod.POST, "/users/signin");
+
+            
+	}
 }

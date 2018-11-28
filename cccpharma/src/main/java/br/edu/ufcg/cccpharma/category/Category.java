4a344d26f7d49b2496c7100c2403d130c7c97dca
Mateus <mateus.oliveira@ccc.ufcg.edu.br>
2018-11-28 10:34:13 -0300

    Update Category
    add id, equals and hashcode

--- cccpharma/src/main/java/br/edu/ufcg/cccpharma/category/Category.java
+++ cccpharma/src/main/java/br/edu/ufcg/cccpharma/category/Category.java
@@ -1,13 +1,16 @@
 package br.edu.ufcg.cccpharma.category;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_category")
 public class Category {
 	
+	@Id
 	private String name;
+	
 	private double discount;
 	
 	
@@ -24,5 +27,28 @@ public class Category {
 	public void setDiscount(double discount) {
 		this.discount = discount;
 	}
-
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 }

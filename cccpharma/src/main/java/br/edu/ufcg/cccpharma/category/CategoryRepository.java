e99b89bd554f70c607faf4bf8636641b707ac063
Mateus Oliveira <33286442+mateusoliveira2@users.noreply.github.com>
2018-11-28 08:39:46 -0300

    Update CategoryRepository.java

--- cccpharma/src/main/java/br/edu/ufcg/cccpharma/category/CategoryRepository.java
+++ cccpharma/src/main/java/br/edu/ufcg/cccpharma/category/CategoryRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+public interface CategoryRepository extends JpaRepository<Category, String> {
 	
 	Category findByName(String name);
 

ef58c3169eaad12d6bf9e1bf07daeaa32046dab6
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2018-12-01 13:13:43 -0300

    Add response status

--- src/main/java/br/edu/ufcg/cccpharma/soldProduct/SoldProductController.java
+++ src/main/java/br/edu/ufcg/cccpharma/soldProduct/SoldProductController.java
@@ -3,7 +3,9 @@ package br.edu.ufcg.cccpharma.soldProduct;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,31 +21,37 @@ public class SoldProductController {
 	private SoldProductService soldProductService;
 
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public SoldProduct save(@RequestBody SoldProduct soldProduct) {
 		return this.soldProductService.save(soldProduct);
 	}
 
 	@GetMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
 	public SoldProduct findById(@PathVariable long id) {
 		return this.soldProductService.findById(id);
 	}
 
 	@GetMapping("/code/{productCode}")
+	@ResponseStatus(HttpStatus.OK)
 	public List<SoldProduct> findByProductCode(@PathVariable String productCode) {
 		return this.soldProductService.findByProductCode(productCode);
 	}
 
 	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
 	public List<SoldProduct> findAll() {
 		return this.soldProductService.findAll();
 	}
 	
 	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
 	public void deleteById(@PathVariable long id) {
 		this.soldProductService.deleteById(id);
 	}
 	
 	@DeleteMapping
+	@ResponseStatus(HttpStatus.OK)
 	public void deleteAll() {
 		this.soldProductService.deleteAll();
 	}

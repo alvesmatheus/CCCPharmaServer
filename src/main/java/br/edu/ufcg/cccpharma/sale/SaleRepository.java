3389e1a7d764ecc0f79bd44688273375d8498b7e
SpinnelSun <m.hollygard@gmail.com>
2018-12-02 16:43:08 -0300

    Add documentation to Sale feature
    Add code documentation to all Sale-related classes and some minor changes.

--- src/main/java/br/edu/ufcg/cccpharma/sale/SaleRepository.java
+++ src/main/java/br/edu/ufcg/cccpharma/sale/SaleRepository.java
@@ -5,6 +5,20 @@ import java.util.List;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * A SaleRepository object is used to create a connection with the database and
+ * provide a simplified way to perform queries about products on it. The methods
+ * signatures here used are self explanatory.
+ * 
+ * @author Douglas Pereira de Lima
+ * @author Fanny Batista Vieira
+ * @author Mateus de Lima Oliveira
+ * @author Matheus Alves dos Santos
+ * 
+ * @since 2018-11-12
+ * @version 1.0
+ * 
+ */
 @Repository
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 

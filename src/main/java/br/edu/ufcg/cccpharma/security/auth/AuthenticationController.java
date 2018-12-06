75849512058ddf62956f3e26a1ea7c99f5aa60e3
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2018-12-05 23:57:17 -0300

    Fix cors problem

--- src/main/java/br/edu/ufcg/cccpharma/security/auth/AuthenticationController.java
+++ src/main/java/br/edu/ufcg/cccpharma/security/auth/AuthenticationController.java
@@ -3,6 +3,7 @@ package br.edu.ufcg.cccpharma.security.auth;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import br.edu.ufcg.cccpharma.security.auth.config.AuthenticationConfig;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping(value="/auth")
 public class AuthenticationController {
 	
